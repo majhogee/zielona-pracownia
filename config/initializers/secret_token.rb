08abd4822f815c05a3ad8bf242d6f61718a2a157
majhogee <jo.wojtowicz@gmail.com>
2013-09-25 00:13:16 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZielonaPracownia::Application.config.secret_key_base = 'b492173510317f922ea285c0cb477e40bcbcdef958038ae810084c2d9a17b70082bb4b89696e884e9e826f99beca6d74ebe0974b98aca672a518cd7e1a067802'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZielonaPracownia::Application.config.session_store :cookie_store, key: '_zielona_pracownia_session'
