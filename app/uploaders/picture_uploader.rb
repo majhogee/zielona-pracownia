f50ee8be27c79f8c13f9cb82a45d707b8f40625f
majhogee <jo.wojtowicz@gmail.com>
2013-10-22 23:27:34 +0200

    Zdjecia dalej nie dzialaja

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,6 +7,16 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
+  process :convert => 'png'
+  process :tags => ['post_picture']
+  
+  version :standard do
+    process :resize_to_fill => [100, 150, :north]
+  end
+  
+  version :thumbnail do
+    resize_to_fit(50, 50)
+  end
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
